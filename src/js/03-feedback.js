22aead10f2099dc2d3bc3f94d17966b67f1e284a
KirillV9819 <kirillv9819@gmail.com>
2022-10-03 10:55:56 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const feedbackFormRef = document.querySelector('.feedback-form');
+
+feedbackFormRef.addEventListener('input', throttle(onFeedbackFormInput, 500));
+feedbackFormRef.addEventListener('submit', onFeedbackFormSubmit);
+
+const {
+  elements: { email, message },
+} = feedbackFormRef;
+
+checkStorage();
+
+function onFeedbackFormInput() {
+  const elementsValue = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(elementsValue));
+}
+
+function checkStorage() {
+  const obj = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (obj) {
+    email.value = obj.email;
+    message.value = obj.message;
+  }
+}
+
+function onFeedbackFormSubmit(evt) {
+  evt.preventDefault();
+
+  localStorage.removeItem('feedback-form-state');
+
+  console.log({
+    email: email.value,
+    message: message.value,
+  });
+  evt.currentTarget.reset();
+}
