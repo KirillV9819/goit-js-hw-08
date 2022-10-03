4909f70fdc7df5acf0567b0210245afde1ce4db0
KirillV9819 <kirillv9819@gmail.com>
2022-10-03 10:55:16 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from "@vimeo/player";
+
+import throttle from "lodash.throttle";   
+
+const iframe = document.querySelector('iframe');
+    
+const player = new Player(iframe);
+
+launchPlayer();
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(evt) {
+  localStorage.setItem('videoplayer-current-time', evt.seconds);
+}
+
+function launchPlayer() {
+  const currTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+  if (currTime) {
+    player.setCurrentTime(time);
+  }
+};
+
 
